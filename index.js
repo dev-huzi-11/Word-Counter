e776bb0924c6bbf67c1249c21abe6a4e4c68b02c
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2024-04-18 18:39:15 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence"
+});
+let word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your sentence word count is ${word.length}`);
